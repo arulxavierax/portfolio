0838af40b4bd80804a9b7dbb7a59b71351316481
arulxavierax <arulxavier2121@gmail.com>
2022-09-18 20:18:28 +0530

    portfolio

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -17,36 +17,36 @@ function Skills() {
           <h1 class=" text-center fw-bold">Skills</h1>
         </Col>
       </Row>
-      <Row>
-        <Col className="skill" sm={1} md={4} lg={3}>
+      <Row md={3} lg={4}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} src={HTML} alt="HTM" />
           <h4 style={{textAlign:"center"}} >HTML</h4>
         </Col>
-        <Col className="skill" sm={1} md={4} lg={3}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} class="mx-auto d-block" src={CSS} alt="CSS" />
           <h4 style={{textAlign:"center"}}>CSS</h4>
         </Col>
-        <Col className="skill" sm={1} md={4} lg={3}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} class="mx-auto d-block" src={JavaScript} alt="Javascript" />
           <h4 style={{textAlign:"center"}}>JavaScript</h4>
         </Col>
-        <Col className="skill" sm={1} md={4} lg={3}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} class="mx-auto d-block" src={ReactLogo} alt="React" />
           <h4 style={{textAlign:"center"}}>React</h4>
         </Col>
-        <Col className="skill" sm={1} md={4} lg={3}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} class="mx-auto d-block" src={MongoDB} alt="MongoDB" />
           <h4 style={{textAlign:"center"}}>MongoDB</h4>
         </Col>
-        <Col className="skill" sm={1} md={4} lg={3}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} class="mx-auto d-block" src={Express} alt="Express" />
           <h4 style={{textAlign:"center"}}>Express</h4>
         </Col>
-        <Col className="skill" sm={1} md={4} lg={3}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} class="mx-auto d-block" src={NodeJS} alt="NodeJS" />
           <h4 style={{textAlign:"center"}}>Node JS</h4>
         </Col>
-        <Col className="skill" sm={1} md={4} lg={3}>
+        <Col className="skill">
           <Image style={{margin:"auto" , display:"block"}} class="mx-auto d-block" src={Git} alt="Github" />
           <h4 style={{textAlign:"center"}}>Github</h4>
         </Col>
